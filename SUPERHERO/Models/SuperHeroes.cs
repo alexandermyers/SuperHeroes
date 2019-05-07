6ab83494630ec3f0c9af72e50264edbb4f979197
unknown <alex.point.maverick@icloud.com>
2019-05-07 15:20:35 -0500

    Initial Commit

---
+++ SUPERHERO/Models/SuperHeroes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SUPERHERO.Models
+{
+    public class SuperHeroes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string HeroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string HeroAbility { get; set; }
+        public string SecondaryHeroAbility { get; set; }
+        public string HeroCatchPhrase { get; set; }
+    }
+}
---
+++ SUPERHERO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SUPERHERO.Startup))]
+namespace SUPERHERO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
